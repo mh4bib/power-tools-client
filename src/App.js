7896c5ae50a764261cdaaf993748a650779305fe
mahiuddinhabib <mh.habib137@gmail.com>
2022-05-23 00:29:29 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/Shared/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
