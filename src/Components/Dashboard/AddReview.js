8517c6af1f5a3aca3f7bb75bd25c8ad0fdb68c1d
mahiuddinhabib <mh.habib137@gmail.com>
2022-05-24 18:40:30 +0600

    reviews added

--- src/Components/Dashboard/AddReview.js
+++ src/Components/Dashboard/AddReview.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const AddReview = () => {
+    const [user] = useAuthState(auth);
+
+    console.log(user);
+    const handleSubmit = event => {
+        event.preventDefault();
+        const name = user.displayName;
+        const picture = user.photoURL;
+        const ratings = event.target.ratings.value;
+        const comment = event.target.review.value;
+        // console.log(tool);
+        const newReview = {name, picture,  comment, ratings};
+
+        const url = 'http://localhost:5000/reviews';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newReview)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    }
+
     return (
-        <div>
-            <h1>add review</h1>
+        <div className='card md:w-[600px] bg-base-100 mx-4 md:mx-auto shadow-xl my-4'>
+            <div className='card-body'>
+                <h3 className="font-bold text-3xl">{}</h3>
+                <form onSubmit={handleSubmit} className='flex flex-col'>
+                    <label class="label">
+                        <span class="label-text">Giv Rating out of 5</span>
+                    </label>
+                    <input type="number" name='ratings' className="input input-bordered w-full mb-[15px]" />
+                    <label class="label">
+                        <span class="label-text">Write Review</span>
+                    </label>
+                    <textarea type="text" name='review' placeholder="Write Review Here" className="textarea textarea-bordered h-24 mb-4" />
+                    <input type="submit" value="submit" className="btn w-full max-w-xs mx-auto"/>
+                </form>
+            </div>
         </div>
     );
 };
--- src/Components/Home/Review.js
+++ src/Components/Home/Review.js
@@ -5,7 +5,7 @@ const Review = ({review}) => {
     return (
         <div className='bg-secondary flex flex-row justify-center items-center p-3 rounded-xl shadow-lg'>
             <img src={picture} alt="" className='w-[70px] rounded-full'/>
-            <div className='ml-2'>
+            <div className='ml-2 overflow-hidden'>
             <h2 className='text-2xl font-semibold'>{name}</h2>
             <p className=''>{comment.slice(0,100)}</p>
             </div>
